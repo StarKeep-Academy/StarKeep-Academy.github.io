e977a9879dd2abeb315e183e8292075f1bcf8f49
danchukal <113303592+danchukal@users.noreply.github.com>
2026-04-05 22:34:26 -0400

    Implemented Profile Page and Subscription pop-up that allows subscription plan change with validation.

--- pages/login/script.js
+++ pages/login/script.js
@@ -2,26 +2,40 @@
 
 
     // Buttons
-        var loginBtn=document.querySelector("#loginButton");
-        loginBtn.addEventListener("click", checkLogin);
+            var loginBtn=document.querySelector("#loginButton");
+            loginBtn.addEventListener("click", checkLogin);
 
-        var manageBtn=document.querySelector("#manageButton");
-        var proBtn=document.querySelector("#proButton");
-        var premiumBtn=document.querySelector("#premiumButton");
-        var closeBtn=document.querySelector("#closeButton");
+            var manageBtn=document.querySelector("#manageButton");
+            manageBtn.addEventListener("click", showPopup);
+
+            var freeBtn = document.querySelector("#freeButton");
+            freeBtn.addEventListener("click", whenClicked);
+            
+            var proBtn=document.querySelector("#proButton");
+            proBtn.addEventListener("click", whenClicked);
+            
+            var premiumBtn=document.querySelector("#premiumButton");
+            premiumBtn.addEventListener("click", whenClicked);
+
+            var freeCol=document.querySelector("#freeColumn");
+            var proCol=document.querySelector("#proColumn");
+            var premiumCol=document.querySelector("#premiumColumn");
+
+            var closeBtn=document.querySelector("#closeButton");
+            closeBtn.addEventListener("click", closePopup);
 
     // Function: Matching Username and Password before logging in
         function checkLogin() {
-        var usernameTyped=document.querySelector("#usernameInput").value;
-        var errorText1=document.querySelector("#error1");
-        var passwordTyped=document.querySelector("#passwordInput").value;
-        var errorText2=document.querySelector("#error2");
-        var successText=document.querySelector("#success");
+            var usernameTyped=document.querySelector("#usernameInput").value;
+            var errorText1=document.querySelector("#error1");
+            var passwordTyped=document.querySelector("#passwordInput").value;
+            var errorText2=document.querySelector("#error2");
+            var successText=document.querySelector("#success");
 
         // Clearing error message when Login button is clicked again
-        errorText1.textContent = "";
-        errorText2.textContent = "";
-        successText.textContent = "";
+            errorText1.textContent = "";
+            errorText2.textContent = "";
+            successText.textContent = "";
 
             if(usernameTyped==="admin" && passwordTyped!=="1234"){
                 errorText2.textContent="Incorrect password";
@@ -35,5 +49,68 @@
             }
             else {
                 successText.textContent="IT WORKED!";
+                document.querySelector("#loginArea").classList.add("hidden");
+                document.querySelector("#profileArea").classList.remove("hidden");            
             }
         }
+
+    // Function to SHOW the popup
+        function showPopup() {
+            document.querySelector("#subscriptionPopup").classList.remove("hidden");
+        }
+
+        function resetPlanStates() {
+            freeCol.style.border="1px solid #ddd";
+            proCol.style.border="1px solid #ddd";
+            premiumCol.style.border="1px solid #ddd";
+
+            freeBtn.style.backgroundColor="";
+            proBtn.style.backgroundColor="";
+            premiumBtn.style.backgroundColor="";     
+
+            freeBtn.value="Change to Free"; 
+            proBtn.value="Change to Pro";
+            premiumBtn.value="Change to Premium";
+            
+            freeBtn.disabled=false;
+            proBtn.disabled=false;
+            premiumBtn.disabled=false;
+        }
+
+    // Function to Upgrade subscription
+        function whenClicked() {
+            resetPlanStates();
+            var ans=document.querySelector("#ansbox");
+            ans.textContent="Subscription Updated!";
+            ans.style.color="green";
+
+            var planText=document.querySelector("#currentPlanText");
+
+            if (this.id==="freeButton") {
+                this.value="Current Plan";              
+                this.disabled=true;    
+                planText.textContent="Current Plan: Free Tier";
+                freeCol.style.border="3px solid green";
+            } 
+
+            else if (this.id==="proButton") {
+                this.value="Current Plan";              
+                this.disabled=true;    
+                planText.textContent="Current Plan: Pro Tier";
+                proCol.style.border="3px solid green";
+            }
+
+            else if (this.id==="premiumButton") {
+                this.value="Current Plan";              
+                this.disabled=true;    
+                planText.textContent="Current Plan: Premium Tier";
+                premiumCol.style.border="3px solid green";
+            }
+
+        }
+
+    // Function to CLOSE the popup
+        function closePopup() {
+            document.querySelector("#subscriptionPopup").classList.add("hidden");
+        }
+
